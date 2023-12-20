886a7f6757411db6719b96b4bdd7b69d3dd038f3
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-12-20 13:15:59 +0530

    Hosting Ready

--- client/src/context/GlobalState.jsx
+++ client/src/context/GlobalState.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import GlobalContext from "./globalContext";
 import io from "socket.io-client";
 
-let url = "http://192.168.29.73:9000";
-const socket = io.connect("http://192.168.29.73:9001");
+let url = import.meta.env.VITE_WEB_URL;
+const socket = io.connect(import.meta.env.VITE_SOCKET_URL);
 
 const GlobalState = (props) => {
   const [data, setData] = useState({
